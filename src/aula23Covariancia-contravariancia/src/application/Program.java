adc4a8eda62cf1396f6114e71984087fa3118aff
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-03 17:03:39 -0300

    final implementation

--- src/aula23Covariancia-contravariancia/src/application/Program.java
+++ src/aula23Covariancia-contravariancia/src/application/Program.java
@@ -2,19 +2,33 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 
 public class Program {
     public static void main(String[] args){
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
         List<Object> myObjs = new ArrayList<Object>();
-        myObjs.add("Maria");
-        myObjs.add("Alex");
-        List<? super Number> myNums = myObjs;
-        myNums.add(10);
-        myNums.add(3.14);
-        Number x = myNums.get(0); // erro de compilacao
 
-//        get - OK
-//        put - ERROR
+        copy(myInts, myObjs);
+        printList(myObjs);
+
+        System.out.println("after ");
+        copy(myDoubles, myObjs);
+        printList(myObjs);
+    }
+
+    public static void copy(List<? extends Number> source, List<? super Number> destiny){
+        for(Number number : source){
+            destiny.add(number);
+        }
+    }
+
+    public static void printList(List<?> list){
+        for(Object obj : list){
+            System.out.print(obj + " ");
+        }
+        System.out.println();
     }
 }
