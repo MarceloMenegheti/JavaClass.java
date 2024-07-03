c370474cb527453632a51493c101a74d7a8e6b13
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-02 22:02:55 -0300

    Generics Delimitados

--- src/Exercicios26GenericsDelimitados/src/application/Program.java
+++ src/Exercicios26GenericsDelimitados/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import entities.Product;
 import services.CalculationService;
 
 import java.io.BufferedReader;
@@ -7,22 +8,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args){
+        Locale.setDefault(Locale.US);
 
-        List<Integer> list = new ArrayList<>();
+        List<Product> list = new ArrayList<>();
 
         String path = "C:\\temp\\in1.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
             String line = br.readLine();
             while(line != null){
-                list.add(Integer.parseInt(line));
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0],Double.parseDouble(fields[1])));
                 line = br.readLine();
             }
 
-            Integer x = CalculationService.max(list);
-            System.out.print("max: ");
+            Product x = CalculationService.max(list);
+            System.out.print("Most expensive: ");
             System.out.println(x);
         }catch (IOException e){
             System.out.println("Error: " + e.getMessage());
--- src/Exercicios26GenericsDelimitados/src/services/CalculationService.java
+++ src/Exercicios26GenericsDelimitados/src/services/CalculationService.java
@@ -1,14 +1,15 @@
 package services;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class CalculationService {
-    public static Integer max(List<Integer> list){
+    public static <T extends Comparable<T>> T max(List<T> list){
         if(list.isEmpty()){
             throw new IllegalArgumentException(("List can't be empty"));
         }
-        Integer max = list.get(0);
-        for(Integer item : list){
+        T max = list.get(0);
+        for(T item : list){
             if(item.compareTo(max) > 0){
                 max = item;
             }
