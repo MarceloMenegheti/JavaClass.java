5262e61c4b6c7d3d4c4010e4eaaf26be1d86c5d4
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-05 20:59:17 -0300

    hashSet

---
+++ src/aula26Set/src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+//import Entities.Product;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Set<String> set = new HashSet<>();
+        set.add("TV");
+        set.add("Tablet");
+        set.add("Notebook");
+
+        System.out.println(set.contains("Notebook"));
+
+        for (String p : set) {
+            System.out.println(p);
+        }
+    }
+}
