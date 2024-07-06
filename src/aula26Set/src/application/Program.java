4f3fd6eb6e1bf53651a5a6a77a2413bd2d949210
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-05 21:06:01 -0300

    LinkedHashSet

--- src/aula26Set/src/application/Program.java
+++ src/aula26Set/src/application/Program.java
@@ -3,17 +3,18 @@ package application;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.LinkedHashSet;
 //import Entities.Product;
 
 public class Program {
     public static void main(String[] args) {
 
-        Set<String> set = new TreeSet<>();
+        Set<String> set = new LinkedHashSet<>();
         set.add("Tv");
         set.add("Tablet");
         set.add("Notebook");
 
-        System.out.println(set.contains("Notebook"));
+        set.removeIf(x -> x.charAt(0) == 'T');
 
         for (String p : set) {
             System.out.println(p);
