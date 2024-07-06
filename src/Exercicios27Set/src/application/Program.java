3cbe4f91f569a2df35389e374ed66090918b3119
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-05 21:23:54 -0300

    Comparação por conteúdo do obj

--- src/Exercicios27Set/src/application/Program.java
+++ src/Exercicios27Set/src/application/Program.java
@@ -10,7 +10,9 @@ public class Program {
         set.add(new Product("TV", 900.0));
         set.add(new Product("Notebook", 1200.0));
         set.add(new Product("Tablet", 400.0));
+
         Product prod = new Product("Notebook", 1200.0);
+
         System.out.println("\nthis product is equal: "+ set.contains(prod));
     }
 }
--- src/Exercicios27Set/src/entities/Product.java
+++ src/Exercicios27Set/src/entities/Product.java
@@ -1,4 +1,7 @@
 package entities;
+
+import java.util.Objects;
+
 public class Product {
     private String name;
     private Double price;
@@ -17,5 +20,18 @@ public class Product {
     }
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
