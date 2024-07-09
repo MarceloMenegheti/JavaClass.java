1ab8e68ddde519468a4ec523fc6eb11155d143b7
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-08 22:22:46 -0300

    Exercicio  - Map

--- src/aula27Map/src/application/Program.java
+++ src/aula27Map/src/application/Program.java
@@ -1,29 +1,24 @@
 package application;
 
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.HashMap;
+import entities.Product;
 
 public class Program {
     public static void main(String[] args){
-        Map<String, String> cookies = new TreeMap<>();
 
-        cookies.put("username", "Maria");
-        cookies.put("email", "maria@gamil.com");
-        cookies.put("phone", "99956446");
+        Map<Product, Double> stock = new HashMap<>();
 
-        cookies.remove("email");
-        cookies.put("phone", "99956336");
+        Product p1 = new Product("Tv", 900.0);
+        Product p2 = new Product("Notebook", 1200.0);
+        Product p3 = new Product("Tablet", 400.0);
 
+        stock.put(p1, 10000.0);
+        stock.put(p2, 20000.0);
+        stock.put(p3, 15000.0);
 
-        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
-        System.out.println("Phone number: " + cookies.get("phone"));
-        System.out.println("Email: " + cookies.get("email"));
-        System.out.println("Size: " + cookies.size());
-
-        System.out.println("ALL COOKIES: ");
-        for(String key : cookies.keySet()){
-            System.out.println(key + ": " + cookies.get(key));
-        }
+        Product ps = new Product("Tv", 900.0);
+        System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
 
     }
 }
