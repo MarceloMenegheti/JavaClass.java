6cb435d7e3c29d7c6265f9f6eb4bb6467e5c2b43
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-13 20:55:03 -0300

    Tipagem dinâmica / inferência de tipos

--- src/aula29ProgramacaoFuncional_CalculoLambda/src/application/Program.java
+++ src/aula29ProgramacaoFuncional_CalculoLambda/src/application/Program.java
@@ -6,20 +6,12 @@ import java.util.Arrays;
 import java.util.List;
 public class Program {
 
-    public static int compareProducts(Product p1, Product p2) {
-        return p1.getPrice().compareTo(p2.getPrice());
-    }
-
     public static void main(String[] args) {
-
         List<Product> list = new ArrayList<>();
-
         list.add(new Product("TV", 900.00));
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
-
-        list.sort(Program::compareProducts);
-
+        list.sort((p1, p2) -> p1.getPrice().compareTo(p2.getPrice()));
         list.forEach(System.out::println);
     }
 }
