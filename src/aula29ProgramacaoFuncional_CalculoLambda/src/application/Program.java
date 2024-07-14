b4ed1d8127d042587f30ea242c517f45673952cc
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-13 21:00:25 -0300

    O que são expressões lambda?

--- src/aula29ProgramacaoFuncional_CalculoLambda/src/application/Program.java
+++ src/aula29ProgramacaoFuncional_CalculoLambda/src/application/Program.java
@@ -1,17 +1,21 @@
 package application;
 import entities.Product;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 public class Program {
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
 
     public static void main(String[] args) {
-        List<Product> list = new ArrayList<>();
-        list.add(new Product("TV", 900.00));
-        list.add(new Product("Notebook", 1200.00));
-        list.add(new Product("Tablet", 450.00));
+        (...)
+
+        list.sort(Program::compareProducts);
+
+        /* Em programação funcional, expressão lambda corresponde a uma
+        função anônima de primeira classe*/
         list.sort((p1, p2) -> p1.getPrice().compareTo(p2.getPrice()));
-        list.forEach(System.out::println);
+
+        (...)
     }
 }
