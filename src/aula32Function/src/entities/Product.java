44880496d81668bba949637a64a8e863b50b4430
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-17 20:33:11 -0300

    Reference method com método estático

--- src/aula32Function/src/entities/Product.java
+++ src/aula32Function/src/entities/Product.java
@@ -27,6 +27,11 @@ public class Product {
     }
 
 
+    public static String staticUpperCase(Product p){
+        return p.getName().toUpperCase();
+    };
+
+
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
