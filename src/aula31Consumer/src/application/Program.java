a1d95a2c7460491c34f30fbfa55024f04e605420
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-17 20:15:12 -0300

    Expressão lambda declarada

--- src/aula31Consumer/src/application/Program.java
+++ src/aula31Consumer/src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 import entities.Product;
 import util.PriceUpdate;
 
-
+import java.util.function.Consumer;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -19,7 +19,13 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        double factor = 1.1;
+
+        Consumer<Product> cons = p -> {
+            p.setPrice(p.getPrice() * factor);
+        };
+
+        list.forEach(cons);
 
         list.forEach(System.out::println);
 
