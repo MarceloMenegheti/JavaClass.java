2f483ca862cc7e45eb6e9e8156b8186e98e7905b
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-18 22:55:26 -0300

    Criando uma Func iteracao/ Seq de Fibonacci

--- src/aula32Stream/src/application/Program.java
+++ src/aula32Stream/src/application/Program.java
@@ -17,5 +17,13 @@ public class Program {
         Stream<String> st2 = Stream.of("Marcelo","Luiz","Diego","Vini");
         System.out.println(Arrays.toString(st2.toArray()));
 
+        //função de iteração
+        Stream<Integer> st3 = Stream.iterate(0,x -> x + 2);//x que leva a x + 2
+        System.out.println(Arrays.toString(st3.limit(10).toArray()));
+
+        //Sequencia de fibonacci
+        Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0] + p[1]}).map(p -> p[0]);
+        System.out.println(Arrays.toString(st4.limit(10).toArray()));
+
     }
 }
