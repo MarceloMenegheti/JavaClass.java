7991c30740cfd8fe739e22bf54678ed8d66591a2
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-09-11 22:46:08 -0300

    Inserir dados/Insert data  - final

--- src/jdbc3/src/application/Program.java
+++ src/jdbc3/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -19,29 +21,44 @@ public class Program {
 		
 		try {
 			conn = DB.getConnection();
-			
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VAlUES "
-					+ "(?, ?, ?, ?, ?)");
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 					
 			st.setString(1, "Karl Marx");
 			st.setString(2, "Karl@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("05/05/1818").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
+			*/
+			
+			st = conn.prepareStatement(
+					"INSERT INTO department (Name) VALUES ('D1'), ('D2')",
+					Statement.RETURN_GENERATED_KEYS);
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rown affected!");
+			}
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
 		}
-		catch(ParseException e) {
-			e.printStackTrace();
-		}
+//		catch(ParseException e) {
+//			e.printStackTrace();
+//		}
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
